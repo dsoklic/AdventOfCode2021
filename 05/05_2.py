55e09afbd7057513421c3aa86d2c55bfcbbacf3f
Domen Soklic <soklicd@gmail.com>
2021-12-05 15:03:53 +0100

    Cleanup

--- 05/05_2.py
+++ 05/05_2.py
@@ -1,12 +1,13 @@
+from collections import defaultdict
+import re
+
 with open('input05.txt', 'r') as f:
    input = [x.strip() for x in f.readlines()]
 
-board = {}
+board = defaultdict(int)
 
 for line in input:
-    start, end = line.split(' -> ')
-    s_x, s_y = [int(i) for i in start.split(',')]
-    e_x, e_y = [int(i) for i in end.split(',')]
+    s_x, s_y, e_x, e_y = [int(x) for x in re.findall(r'\d+', line)]
 
     diagonal = s_x != e_x and s_y != e_y
 
@@ -23,16 +24,10 @@ for line in input:
     if not diagonal:
         for x in x_range:
             for y in y_range:
-                if (x, y) in board:
-                    board[(x, y)] += 1
-                else:
-                    board[(x, y)] = 1
+                board[(x, y)] += 1
     else:
         for x,y in zip(x_range, y_range):
-            if (x, y) in board:
-                board[(x, y)] += 1
-            else:
-                board[(x, y)] = 1  
+            board[(x, y)] += 1
 
 num_intersect = sum([intersects >= 2 for intersects in board.values()])
 
