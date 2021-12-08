27baefb49d3288bb4b3174c713a813cfd94afede
Domen Soklic <soklicd@gmail.com>
2021-12-08 19:23:16 +0100

    Cleanup

--- 08/08_2.py
+++ 08/08_2.py
@@ -13,18 +13,18 @@ def issubset(a, b):
 def get_number_mappings(combinations: List[str]):
     segments = [{'a','b','c','d','e','f','g'}]*10
 
-    segments[1] = next(v for i,v in enumerate(combinations) if len(v) == 2)
-    segments[4] = next(v for i,v in enumerate(combinations) if len(v) == 4)
-    segments[7] = next(v for i,v in enumerate(combinations) if len(v) == 3)
+    segments[1] = next(v for v in combinations if len(v) == 2)
+    segments[4] = next(v for v in combinations if len(v) == 4)
+    segments[7] = next(v for v in combinations if len(v) == 3)
 
-    segments[3] = next(v for i,v in enumerate(combinations) if len(v) == 5 and issubset(segments[7], v))
+    segments[3] = next(v for v in combinations if len(v) == 5 and issubset(segments[7], v))
 
-    segments[9] = next(v for i,v in enumerate(combinations) if len(v) == 6 and issubset(segments[3], v))
-    segments[0] = next(v for i,v in enumerate(combinations) if len(v) == 6 and issubset(segments[7], v) and v != segments[9])
-    segments[6] = next(v for i,v in enumerate(combinations) if len(v) == 6 and v != segments[9] and v != segments[0])
+    segments[9] = next(v for v in combinations if len(v) == 6 and issubset(segments[3], v))
+    segments[0] = next(v for v in combinations if len(v) == 6 and issubset(segments[7], v) and v != segments[9])
+    segments[6] = next(v for v in combinations if len(v) == 6 and v != segments[9] and v != segments[0])
 
-    segments[5] = next(v for i,v in enumerate(combinations) if len(v) == 5 and issubset(v, segments[6]))
-    segments[2] = next(v for i,v in enumerate(combinations) if len(v) == 5 and v != segments[3] and v != segments[5])
+    segments[5] = next(v for v in combinations if len(v) == 5 and issubset(v, segments[6]))
+    segments[2] = next(v for v in combinations if len(v) == 5 and v != segments[3] and v != segments[5])
 
     return [set(x) for x in segments]
 
