5eff2fe7c27c60cfdab2026c74400e18d103826e
Domen Soklic <soklicd@gmail.com>
2021-12-12 16:26:17 +0100

    Day 11

---
+++ 11/11_2.py
@@ -0,0 +1,68 @@
+from queue import SimpleQueue
+
+# input = ["5483143223","2745854711","5264556173","6141336146","6357385478","4167524645","2176841721","6882881134","4846848554","5283751526"]
+
+with open('input11.txt', 'r') as f:
+    input = [x.strip() for x in f.readlines()]
+input = [list(map(int,i)) for i in [list(x) for x in input]]
+
+def legal(x, y):
+    global input
+    return x > -1 and x < len(input[0]) and y > -1 and y < len(input)
+
+def increase(flashed, x, y):
+    global input
+    if legal(x, y):
+        input[y][x] += 1
+
+        if input[y][x] == 10:
+            flashed.append((x,y))
+
+def increase_surounding(x, y):
+    global input
+
+    flashed = []
+
+    increase(flashed, x-1, y-1)
+    increase(flashed, x-1, y)
+    increase(flashed, x-1, y+1)
+    increase(flashed, x, y-1)
+    increase(flashed, x, y)
+    increase(flashed, x, y+1)
+    increase(flashed, x+1, y-1)
+    increase(flashed, x+1, y)
+    increase(flashed, x+1, y+1)
+
+    return flashed
+
+flashes = 0
+step = 0
+while True:
+    step += 1
+    to_flash = SimpleQueue()
+
+    for y in range(len(input)):
+        for x in range(len(input[0])):
+            input[y][x] += 1
+
+            if input[y][x] > 9:
+                to_flash.put((x,y))
+
+    # simulate all flashes
+    while not to_flash.empty():
+        x, y = to_flash.get()
+        new_flash = increase_surounding(x,y)
+        [to_flash.put(fl) for fl in new_flash]
+
+    all_would_flash = True
+    for y in range(len(input)):
+        for x in range(len(input[0])):
+            if input[y][x] > 9:
+                input[y][x] = 0
+                flashes += 1
+            else:
+                all_would_flash = False
+
+    if all_would_flash:
+        print(step)
+        break
