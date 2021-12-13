95cb5bd5478d37f76c149b1fe1d594fd758b7e7d
Domen Soklic <soklicd@gmail.com>
2021-12-13 19:18:52 +0100

    Day 12

---
+++ 12/12_2.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+from queue import SimpleQueue
+
+# input = ["start-A","start-b","A-c","A-b","b-d","A-end","b-end"]
+# input = ["dc-end","HN-start","start-kj","dc-start","dc-HN","LN-dc","HN-end","kj-sa","kj-HN","kj-dc"]
+
+with open('input12.txt', 'r') as f:
+    input = [x.strip() for x in f.readlines()]
+
+graph = defaultdict(lambda: set())
+for connection in input:
+    a, b = connection.split('-')
+    graph[a].add(b)
+    graph[b].add(a)
+
+def already_wisited_small(history, next_node):
+    if next_node.isupper(): 
+        return False
+    if next_node == 'start':
+        return True
+    if next_node == 'end':
+        return next_node in history
+
+    # Any node already visited twice?
+    counter = defaultdict(int)
+    for node in history:
+        counter[node] += 1
+    
+    if any([v >= 2 for k,v in counter.items() if k.islower()]):
+        return next_node in history
+    else:
+        return False
+
+paths = SimpleQueue()
+paths.put(['start'])
+finished_paths = []
+
+while not paths.empty():
+    path = paths.get()
+
+    next_nodes = graph[path[-1]]
+    next_nodes = list(filter(lambda x: not already_wisited_small(path, x), next_nodes))
+
+    for next_node in next_nodes:
+        new_path = path.copy()
+        new_path.append(next_node)
+        if next_node == 'end':
+            finished_paths.append(new_path)
+        else:
+            paths.put(new_path)
+
+print(len(finished_paths))
